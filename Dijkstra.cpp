0430a52061e7dfde4f113a8cd8b7b139598a646a
flyhero <921285003@qq.com>
2018-10-31 19:57:32 +0800

    update

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,67 +1,62 @@
 #include <bits/stdc++.h>
-#define MOD 1000000007
+#define mem(a, b) memset(a, b, sizeof a)
 #define inf 0x3f3f3f3f
-#define mem(a, b) memset(a, b, sizeof(a))
+typedef long long ll;
+const int maxn = 200005;
 using namespace std;
 
-const int maxn = 200005;
-
-int n, m;
-int st, ed;
+int n, m, st, ed;
 
 struct qnode {
     int v, c;
-    qnode(int v = 0, int c = 0) : v(v), c(c) {}
+    qnode(int _v = 0, int _c = 0) : v(_v), c(_c) {}
     bool operator < (const qnode &r) const {
-        return c > r.c ;
+        return c > r.c;
     }
 };
 
 struct Edge {
-    int start, cost;
-    Edge(int start = 0 , int cost = 0) : start(start), cost(cost) {}
-
+    int s, c;
+    Edge(int _s = 0, int _c = 0) : s(_s), c(_c) {}
 };
 
-vector <Edge> vec[maxn] ;
-bool vis[maxn];
+vector<Edge> vec[maxn];
 int dist[maxn];
+bool vis[maxn];
+
+void addEdge(int u, int v, int w) {
+    vec[u].push_back(Edge(v, w));
+    vec[v].push_back(Edge(u, w));
+}
 
 void dijk(int st) {
-    mem(vis, 0);
-    for(int i = 0; i <= n; i++) dist[i] = inf;
-    priority_queue<qnode> pq; while(!pq.empty()) pq.pop();
-    dist[st] = 0; pq.push(qnode(st, 0));
+    mem(vis, 0); for(int i = 0; i <= n; i++) dist[i] = inf;
+    dist[st] = 0;
+    priority_queue<qnode> q; q.push(qnode(st, 0));
     qnode tmp;
-    while(!pq.empty()) {
-        tmp = pq.top(); pq.pop();
+    while(!q.empty()) {
+        tmp = q.top(); q.pop();
         int u = tmp.v;
-        if(vis[u]) continue;
-        vis[u] = true;
-        for(int i = 0; i < vec[u].size(); i++) {
-            int v = vec[u][i].start;
-            int cost = vec[u][i].cost;
-            if(!vis[v]) {
-                dist[v] = min(dist[v], dist[u]+cost);
-                pq.push(qnode(v, dist[v]));
+        if(!vis[u]) {
+            vis[u] = true;
+            for(int i = 0; i < vec[u].size(); i++) {
+                int v = vec[u][i].s, c = vec[u][i].c;
+                if(!vis[v]) {
+                    dist[v] = min(dist[v], dist[u]+c);
+                    q.push(qnode(v, dist[v]));
+                }
             }
         }
     }
 }
 
-void addedge(int u, int v, int w) { // µ¥Ïò¼Ó±ß
-    vec[u].push_back(Edge(v,w));
-}
-
 int main() {
-    int cas; cin >> cas;
-    while(cas--) {
-        scanf("%d %d", &n, &m);
+    while(~scanf("%d %d", &n, &m) && (n+m)) {
         for(int i = 0; i <= n; i++) vec[i].clear();
-        int u, v, w;
         for(int i = 0; i < m; i++) {
+            int u, v, w;
             scanf("%d %d %d", &u, &v, &w);
-            addedge(u, v, w);
+            addEdge(u, v, w);
         }
         st = 1, ed = n;
         dijk(st);
